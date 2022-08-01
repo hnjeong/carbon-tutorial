17fda75084e52a1058283a8530ee1e94f98f8d21
hnjeong <hnjeong@seegene.com>
2022-08-01 10:19:16 +0900

    added grid

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -1,7 +1,40 @@
+import { Grid, Column } from '@carbon/react';
 import React from 'react';
 
 const LandingPage = () => {
-  return <div>LANDING PAGE</div>;
+  return (
+    <Grid className="landing-page" fullWidth>
+      <Column lg={16} md={8} sm={4} className="landing-page__banner">
+        1
+      </Column>
+      <Column lg={16} md={8} sm={4} className="landing-page__r2">
+        <Grid className="tabs-group-content">
+          <Column md={4} lg={7} sm={4} className="landing-page__tab-content">
+            7/16
+          </Column>
+          <Column md={4} lg={{ span: 8, offset: 7 }} sm={4}>
+            8/16
+          </Column>
+        </Grid>
+      </Column>
+      <Column lg={16} md={8} sm={4} className="landing-page__r3">
+        <Grid>
+          <Column md={4} lg={4} sm={4}>
+            1/4
+          </Column>
+          <Column md={4} lg={4} sm={4}>
+            1/4
+          </Column>
+          <Column md={4} lg={4} sm={4}>
+            1/4
+          </Column>
+          <Column md={4} lg={4} sm={4}>
+            1/4
+          </Column>
+        </Grid>
+      </Column>
+    </Grid>
+  );
 };
 
 export default LandingPage;
